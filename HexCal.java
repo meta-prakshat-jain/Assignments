0c362c164c009b510e6faedd9f9afc0905b19a8d
meta-prakshat-jain <prakshat.jain@metacube.com>
2025-03-10 18:27:40 +0530

    Update HexCal.java

--- HexCal.java
+++ HexCal.java
@@ -1,231 +1,278 @@
 package Assignment2;
-
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Scanner;
- class HexCal{
-	 /**
-	  * THis Method is converting our hexadecimal string to decimal
-	  * @param hex
-	  * @return decimal
-	  */
-	public static int HexToDec(String hex) {
-		hex=hex.toUpperCase();
-		int decimal=0;
-		int power=0;
-		for(int i=hex.length()-1;i>=0;i--) {
-			char currChar=hex.charAt(i);
-			int digitValue;
-			if(currChar>='0' && currChar<='9') {
-				digitValue=currChar-'0';
-			}else if(currChar>='A' && currChar<='F') {
-				digitValue=currChar-'A'+10;//Convert char A-F to integer
-			}else {
-				throw new IllegalArgumentException("Invalid hexadecimal character: "+currChar);
-			}
-			decimal+=digitValue*Math.pow(16, power);
-			power++;
-		}
-		return decimal;
-	}
-	
-	public static String decimalToHex(int decimal) {
-		if(decimal==0) {
-			return  "0";			
-		}
-		StringBuilder hex=new StringBuilder();
-		while(decimal>0) {
-			int remainder=decimal%16;
-			char hexChar;
-			if(remainder<10) {
-				hexChar=(char)('0'+remainder);//Convert 0-9 to character 0-9
-			}else {
-				hexChar=(char)('A'+(remainder-10));//Convert 10-15 to A-F
-			}
-			hex.append(hexChar);
-			decimal/=16;
-		}
-		return hex.reverse().toString();
-	}
-	
-		public String add(String hex1,String hex2) {
-			return decimalToHex(HexToDec(hex1)+HexToDec(hex2));
-		}
-		public String subtract(String hex1,String hex2) {
-			int dec1=HexToDec(hex1);
-			int dec2=HexToDec(hex2);
-			if(dec2>dec1) {
-				return"Error: Negative Result not supported";
-			}
-			return decimalToHex(dec1-dec2);
-		}
-		public String multiply(String hex1,String hex2) {
-			return decimalToHex(HexToDec(hex1)*HexToDec(hex2));
-		}
-		public String Divide(String hex1,String hex2) {
-			int dec1=HexToDec(hex1);
-			int dec2=HexToDec(hex2);
-			if(dec2>dec1) {
-				return"Error: Divison by Zero";
-			}
-			return decimalToHex(dec1/dec2);
-		}
-	public boolean equals(String hex1,String hex2) {
-		hex1=hex1.toUpperCase();
-		hex2=hex2.toUpperCase();
-		return hex1.equals(hex2);
-	}
-	public boolean greaterThan(String hex1,String hex2) {
-		hex1=hex1.toUpperCase();
-		hex2=hex2.toUpperCase();
-		int len1=hex1.length();
-		int len2=hex2.length();
-		if(len1>len2) {
-			return true;
-		}else if(len2>len1) {
-			return false;
-		}
-		for(int i=0;i<len1;i++) {
-			char char1=hex1.charAt(i);
-			char char2=hex2.charAt(i);
-			
-			if(char1>char2) {
-				return true;
-			}else if(char2>char1) {
-				return false;
-			}
-		}
-		return false;
-	}
-	public boolean lessThan(String hex1,String hex2) {
-		hex1=hex1.toUpperCase();
-		hex2=hex2.toUpperCase();
-		int len1=hex1.length();
-		int len2=hex2.length();
-		if(len1<len2) {
-			return true;
-		}else if(len2<len1) {
-			return false;
-		}
-		for(int i=0;i<len1;i++) {
-			char char1=hex1.charAt(i);
-			char char2=hex2.charAt(i);
-			
-			if(char1<char2) {
-				return true;
-			}else if(char2<char1) {
-				return false;
-			}
-		}
-		return false;
-	}
 
-	public static void main(String[] args) {
-//		String hexNumber="1A3";
-//		int decimalEquivalent=HexToDec(hexNumber);
-//		System.out.println("Hexadecimal "+hexNumber+" = Decimal "+decimalEquivalent);
-//		int decimalNumber=419;
-//		String hex=decimalToHex(decimalNumber);
-//		System.out.println("Decimal "+decimalNumber+" = Hexadecimal "+hex);
-		 HexCal calc = new HexCal();
-	        Scanner scanner = new Scanner(System.in);
-	        int choice;
+class HexCal {
+    
+	/**
+     * Converts a  hexadecimal to decimal.
+     * 1st converting string to upper case 
+     * if character is between 0 to 9 then decreasing ascii value of 0
+     * if character is between A to F then decreasing ascii value of A +10
 
-	            System.out.println("\nHexadecimal Calculator Menu:");
-	            System.out.println("1. Add");
-	            System.out.println("2. Subtract");
-	            System.out.println("3. Multiply");
-	            System.out.println("4. Divide");
-	            System.out.println("5. Equals");
-	            System.out.println("6. Greater Than");
-	            System.out.println("7. Less Than");
-	            System.out.println("8. Hex to Decimal");
-	            System.out.println("9. Decimal to Hex");
-	            System.out.println("0. Exit");
-	            System.out.print("Enter your choice: ");
+     */
+    public static int hexToDec(String hex) {
+        try {
+            hex = hex.toUpperCase();
+            int decimal = 0;
+            int power = 0;
 
-	            choice = scanner.nextInt();
-	            scanner.nextLine(); // Consume newline
+            for (int i = hex.length() - 1; i >= 0; i--) {
+                char currChar = hex.charAt(i);
+                int digitValue;
 
-	            String hex1, hex2, result;
-	            int decimal, resultDecimal;
-	            boolean comparisonResult;
+                if (currChar >= '0' && currChar <= '9') {
+                    digitValue = currChar - '0';
+                } else if (currChar >= 'A' && currChar <= 'F') {
+                    digitValue = currChar - 'A' + 10; 
+                } else {
+                    throw new IllegalArgumentException("Invalid hexadecimal character: " + currChar);
+                }
 
-	            switch (choice) {
-	                case 1:
-	                    System.out.print("Enter first hexadecimal number: ");
-	                    hex1 = scanner.nextLine();
-	                    System.out.print("Enter second hexadecimal number: ");
-	                    hex2 = scanner.nextLine();
-	                    result = calc.add(hex1, hex2);
-	                    System.out.println("Result: " + result);
-	                    break;
-	                case 2:
-	                    System.out.print("Enter first hexadecimal number: ");
-	                    hex1 = scanner.nextLine();
-	                    System.out.print("Enter second hexadecimal number: ");
-	                    hex2 = scanner.nextLine();
-	                    result = calc.subtract(hex1, hex2);
-	                    System.out.println("Result: " + result);
-	                    break;
-	                case 3:
-	                    System.out.print("Enter first hexadecimal number: ");
-	                    hex1 = scanner.nextLine();
-	                    System.out.print("Enter second hexadecimal number: ");
-	                    hex2 = scanner.nextLine();
-	                    result = calc.multiply(hex1, hex2);
-	                    System.out.println("Result: " + result);
-	                    break;
-	                case 4:
-	                    System.out.print("Enter first hexadecimal number: ");
-	                    hex1 = scanner.nextLine();
-	                    System.out.print("Enter second hexadecimal number: ");
-	                    hex2 = scanner.nextLine();
-	                    result = calc.Divide(hex1, hex2);
-	                    System.out.println("Result: " + result);
-	                    break;
-	                case 5:
-	                    System.out.print("Enter first hexadecimal number: ");
-	                    hex1 = scanner.nextLine();
-	                    System.out.print("Enter second hexadecimal number: ");
-	                    hex2 = scanner.nextLine();
-	                    comparisonResult = calc.equals(hex1, hex2);
-	                    System.out.println("Result: " + comparisonResult);
-	                    break;
-	                case 6:
-	                    System.out.print("Enter first hexadecimal number: ");
-	                    hex1 = scanner.nextLine();
-	                    System.out.print("Enter second hexadecimal number: ");
-	                    hex2 = scanner.nextLine();
-	                    comparisonResult = calc.greaterThan(hex1, hex2);
-	                    System.out.println("Result: " + comparisonResult);
-	                    break;
-	                case 7:
-	                    System.out.print("Enter first hexadecimal number: ");
-	                    hex1 = scanner.nextLine();
-	                    System.out.print("Enter second hexadecimal number: ");
-	                    hex2 = scanner.nextLine();
-	                    comparisonResult = calc.lessThan(hex1, hex2);
-	                    System.out.println("Result: " + comparisonResult);
-	                    break;
-	                case 8:
-	                    System.out.print("Enter hexadecimal number: ");
-	                    hex1 = scanner.nextLine();
-	                    decimal = calc.HexToDec(hex1);
-	                    System.out.println("Result: " + decimal);
-	                    break;
-	                case 9:
-	                    System.out.print("Enter decimal number: ");
-	                    decimal = scanner.nextInt();
-	                    scanner.nextLine(); // Consume newline
-	                    result = calc.decimalToHex(decimal);
-	                    System.out.println("Result: " + result);
-	                    break;
-	                case 0:
-	                    System.out.println("Exiting...");
-	                    break;
-	                default:
-	                    System.out.println("Invalid choice. Please try again.");
-	            }
-	    }
-	}
+                decimal += digitValue * Math.pow(16, power);
+                power++;
+            }
+            return decimal;
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+            return -1; 
+        }
+    }
+
+    /**
+     * Converts a decimal integer to a hexadecimal string.
+     * Checkin if decimal is less then 0 
+     * if yes throw error
+     * if decimal==0 then return 0
+     * Making an string builder to convert decimal to hexadecimal
+     */
+    public static String decimalToHex(int decimal) {
+        if (decimal < 0) {
+            return "Error: Negative numbers are not supported";
+        }
+        if (decimal == 0) {
+            return "0";
+        }
+
+        StringBuilder hex = new StringBuilder();
+        while (decimal > 0) {
+            int remainder = decimal % 16;
+            char hexChar = (char) ((remainder < 10) ? ('0' + remainder) : ('A' + (remainder - 10)));
+            hex.append(hexChar);
+            decimal /= 16;
+        }
+        return hex.reverse().toString();
+    }
+    /**
+     * Helps to add two hexadecimal 
+     * 1st:- We are converting hexadecimal to decimal both the strings
+     * 2nd:-THen we are converting the whole answer into decimal to hexadecimal
+     * @param hex1
+     * @param hex2
+     * @return adding 
+     */
+    public String add(String hex1, String hex2) {
+        return decimalToHex(hexToDec(hex1) + hexToDec(hex2));
+    }
+    /**
+     * Helps to Subtract two hexadecimal 
+     * 1st:- We are converting hexadecimal to decimal both the strings
+     * 2nd:-Ten we are converting the whole answer into decimal to hexadecimal
+     * @param hex1
+     * @param hex2
+     * @return subtract
+     * we are also checking if if answer is not getting negative 
+     */
+    public String subtract(String hex1, String hex2) {
+        int dec1 = hexToDec(hex1);
+        int dec2 = hexToDec(hex2);
+        if (dec1 < dec2) {
+            return "Error: Negative result not supported";
+        }
+        return decimalToHex(dec1 - dec2);
+    }
+    /**
+     * Helps to Multiply two hexadecimal 
+     * 1st:- We are converting hexadecimal to decimal both the strings
+     * 2nd:-Then we are converting the whole answer into decimal to hexadecimal
+     * @param hex1
+     * @param hex2
+     * @return Multiply 
+     */
+
+    public String multiply(String hex1, String hex2) {
+        return decimalToHex(hexToDec(hex1) * hexToDec(hex2));
+    }
+    /**
+     * Helps to divide two hexadecimal 
+     * 1st:- We are converting hexadecimal to decimal both the strings
+     * 2nd:-THen we are converting the whole answer into decimal to hexadecimal
+     * @param hex1
+     * @param hex2
+     * @return divide 
+     * we are also checking if if answer is not getting negative 
+     */
+    public String divide(String hex1, String hex2) {
+        int dec1 = hexToDec(hex1);
+        int dec2 = hexToDec(hex2);
+        if (dec2 == 0) {
+            return "Error: Division by zero is not allowed";
+        }
+        return decimalToHex(dec1 / dec2);
+    }
+/**
+ * Checking if two hexadecimals are equal or not 
+ * By converting them and checking if they are equal or not 
+ * And returning true if equal
+ * @param hex1
+ * @param hex2
+ * @return
+ */
+    public boolean equals(String hex1, String hex2) {
+        return hexToDec(hex1) == hexToDec(hex2);
+    }
+    /**
+     * Checking if two hexadecimals which is greater
+     * By converting them and checking which is greater which is smaller
+     * And returning true for greater one
+     * @param hex1
+     * @param hex2
+     * @return Greater Hexa decimal
+     */
+    public boolean greaterThan(String hex1, String hex2) {
+        return hexToDec(hex1) > hexToDec(hex2);
+    }
+    /**
+     * Checking the smaller hexadecimals between these two
+     * By converting them and checking which is smaller 
+     * And returning smaller one
+     * @param hex1
+     * @param hex2
+     * @return less hexadecimal
+     */
+
+    public boolean lessThan(String hex1, String hex2) {
+        return hexToDec(hex1) < hexToDec(hex2);
+    }
+
+    public static void main(String[] args) {
+        HexCal calc = new HexCal();
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            try {
+                System.out.println("\nHexadecimal Calculator Menu:");
+                System.out.println("1. Add");
+                System.out.println("2. Subtract");
+                System.out.println("3. Multiply");
+                System.out.println("4. Divide");
+                System.out.println("5. Equals");
+                System.out.println("6. Greater Than");
+                System.out.println("7. Less Than");
+                System.out.println("8. Hex to Decimal");
+                System.out.println("9. Decimal to Hex");
+                System.out.println("0. Exit");
+                System.out.print("Enter your choice: ");
+
+                int choice = scanner.nextInt();
+                scanner.nextLine(); 
+
+                String hex1, hex2, result;
+                int decimal;
+                boolean comparisonResult;
+
+                switch (choice) {
+                    case 1:
+                        System.out.print("Enter first hexadecimal number: ");
+                        hex1 = scanner.nextLine();
+                        System.out.print("Enter second hexadecimal number: ");
+                        hex2 = scanner.nextLine();
+                        result = calc.add(hex1, hex2);
+                        System.out.println("Result: " + result);
+                        break;
+
+                    case 2:
+                        System.out.print("Enter first hexadecimal number: ");
+                        hex1 = scanner.nextLine();
+                        System.out.print("Enter second hexadecimal number: ");
+                        hex2 = scanner.nextLine();
+                        result = calc.subtract(hex1, hex2);
+                        System.out.println("Result: " + result);
+                        break;
+
+                    case 3:
+                        System.out.print("Enter first hexadecimal number: ");
+                        hex1 = scanner.nextLine();
+                        System.out.print("Enter second hexadecimal number: ");
+                        hex2 = scanner.nextLine();
+                        result = calc.multiply(hex1, hex2);
+                        System.out.println("Result: " + result);
+                        break;
+
+                    case 4:
+                        System.out.print("Enter first hexadecimal number: ");
+                        hex1 = scanner.nextLine();
+                        System.out.print("Enter second hexadecimal number: ");
+                        hex2 = scanner.nextLine();
+                        result = calc.divide(hex1, hex2);
+                        System.out.println("Result: " + result);
+                        break;
+
+                    case 5:
+                        System.out.print("Enter first hexadecimal number: ");
+                        hex1 = scanner.nextLine();
+                        System.out.print("Enter second hexadecimal number: ");
+                        hex2 = scanner.nextLine();
+                        comparisonResult = calc.equals(hex1, hex2);
+                        System.out.println("Are they equal? " + comparisonResult);
+                        break;
+
+                    case 6:
+                        System.out.print("Enter first hexadecimal number: ");
+                        hex1 = scanner.nextLine();
+                        System.out.print("Enter second hexadecimal number: ");
+                        hex2 = scanner.nextLine();
+                        comparisonResult = calc.greaterThan(hex1, hex2);
+                        System.out.println("Is first greater than second? " + comparisonResult);
+                        break;
+
+                    case 7:
+                        System.out.print("Enter first hexadecimal number: ");
+                        hex1 = scanner.nextLine();
+                        System.out.print("Enter second hexadecimal number: ");
+                        hex2 = scanner.nextLine();
+                        comparisonResult = calc.lessThan(hex1, hex2);
+                        System.out.println("Is first less than second? " + comparisonResult);
+                        break;
+
+                    case 8:
+                        System.out.print("Enter hexadecimal number: ");
+                        hex1 = scanner.nextLine();
+                        decimal = hexToDec(hex1);
+                        if (decimal != -1) {
+                            System.out.println("Decimal equivalent: " + decimal);
+                        }
+                        break;
+
+                    case 9:
+                        System.out.print("Enter decimal number: ");
+                        decimal = scanner.nextInt();
+                        scanner.nextLine(); // Consume newline
+                        result = decimalToHex(decimal);
+                        System.out.println("Hexadecimal equivalent: " + result);
+                        break;
+
+                    case 0:
+                        System.out.println("Exiting...");
+                        scanner.close();
+                        return;
+
+                    default:
+                        System.out.println("Invalid choice. Please try again.");
+                }
+            } catch (Exception e) {
+                System.out.println("Invalid input. Please enter valid hexadecimal or decimal values.");
+                scanner.nextLine(); 
+            }
+        }
+    }
+}
